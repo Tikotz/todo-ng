6dc34ce9e874c76670cae8ae2ca2a563b63eee96
ori tikozki <oritikozki@gmail.com>
2022-03-21 21:13:24 +0200

    added updates

--- src/app/todo2/todo2.component.ts
+++ src/app/todo2/todo2.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { filter } from 'rxjs';
 
+export interface Task{
+  name:string;
+  isUpdated:boolean;
+}
+
+
 @Component({
   selector: 'app-todo2',
   templateUrl: './todo2.component.html',
@@ -9,23 +15,32 @@ import { filter } from 'rxjs';
 })
 export class Todo2Component implements OnInit {
 
-  tasks:string[] = [];
+  tasks:Task[] = [];
   constructor() { 
-
-    
-    
   }
 
   ngOnInit(): void {
   }
 
   handleSubmit(addForm:NgForm){
-    let newTask = addForm.value.task;
+    let newTask:Task = { name: addForm.value.task, isUpdated: false};
     this.tasks.push(newTask);
     addForm.resetForm();
   }
 
   handleRemove(t:string){
-    this.tasks = this.tasks.filter((myTask) => myTask != t);
+    this.tasks = this.tasks.filter((myTask:Task) => myTask.name != t);
   }
+
+  handleUpdate(t:Task){
+    t.isUpdated = true;
+  }
+
+  handleFinishUpdate(oldName:string,newTaskName:string){
+    let updatedTask:Task = this.tasks.filter( (t) => t.name === oldName )[0];
+    updatedTask.name = newTaskName;
+    updatedTask.isUpdated = false;
+  }
+
+
 }
